06d0b5dfdea63af3cfcee0e8a2d7d9728ddfbde9
AcharyaUjjal1 <acharya_ujjal@yahoo.com>
2019-09-04 21:13:58 +0545

    refactor bus route for RESTFUL convention

--- routes/bus.js
+++ routes/bus.js
@@ -2,10 +2,13 @@ const express = require("express");
 const router = express.Router();
 const { requireOwnerSignin } = require("../controllers/auth-owner");
 const { read, create, busBySlug, getBuses } = require("../controllers/bus");
-const {uploadBusImage} = require("../helpers/multer");
+const { uploadBusImage } = require("../helpers/multer");
 
-router.get("/", getBuses);
-router.post("/add", requireOwnerSignin, uploadBusImage, create);
+router
+  .route("/")
+  .get(getBuses)
+  .post(requireOwnerSignin, uploadBusImage, create);
+
 router.get("/:busSlug", read);
 
 router.param("busSlug", busBySlug);
