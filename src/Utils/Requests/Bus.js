89b2373d8bb1105ebb8e0911d21e2b709cc1cf38
AcharyaUjjal1 <acharya_ujjal@yahoo.com>
2019-09-08 12:12:57 +0545

    add unavailable-bus data table

--- src/Utils/Requests/Bus.js
+++ src/Utils/Requests/Bus.js
@@ -1,3 +1,4 @@
 import axios from 'axios';
 
 export const getAvailableBusesOfOwner = () => axios.get('/bus/bus-available');
+export const getUnavailableBusesOfOwner = () => axios.get('/bus/bus-unavailable');
