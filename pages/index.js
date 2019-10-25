242d072a2cb29261970b789c458dd44d41b4bc78
AcharyaUjjal1 <acharya_ujjal@yahoo.com>
2019-10-25 17:06:04 +0545

    make locations searchable

--- pages/index.js
+++ pages/index.js
@@ -13,11 +13,26 @@ function handleChange(value) {
 	console.log(`selected ${value}`);
 }
 
-function disabledDate(current) {
-  // Can not select days before today and today
-  return current && current < moment().endOf('day');
+function onChange(value) {
+	console.log(`selected ${value}`);
 }
 
+function onBlur() {
+	console.log('blur');
+}
+
+function onFocus() {
+	console.log('focus');
+}
+
+function onSearch(val) {
+	console.log('search:', val);
+}
+
+function disabledDate(current) {
+	// Can not select days before today and today
+	return current && current < moment().endOf('day');
+}
 
 const Home = () => (
 	<div>
@@ -38,9 +53,18 @@ const Home = () => (
 								<h4 className="color-white">From: </h4>
 							</label>
 							<Select
+								showSearch
 								defaultValue="dhn"
-								style={{ width: 150, marginRight: '1rem' }}
-								onChange={handleChange}
+								style={{ width: 200, marginRight: '1rem' }}
+								placeholder="Select a person"
+								optionFilterProp="children"
+								onChange={onChange}
+								onFocus={onFocus}
+								onBlur={onBlur}
+								onSearch={onSearch}
+								filterOption={(input, option) =>
+									option.props.children.toLowerCase().indexOf(input.toLowerCase()) >= 0
+								}
 							>
 								<Option value="dhn">Dhangadhi</Option>
 								<Option value="mnr">Mahendranagar</Option>
@@ -49,21 +73,26 @@ const Home = () => (
 								<h4 className="color-white">To: </h4>
 							</label>
 							<Select
-								defaultValue="dhn"
-								style={{ width: 150, marginRight: '1rem' }}
-								onChange={handleChange}
+								showSearch
+								defaultValue="ktm"
+								style={{ width: 200, marginRight: '1rem' }}
+								placeholder="Select a person"
+								optionFilterProp="children"
+								onChange={onChange}
+								onFocus={onFocus}
+								onBlur={onBlur}
+								onSearch={onSearch}
+								filterOption={(input, option) =>
+									option.props.children.toLowerCase().indexOf(input.toLowerCase()) >= 0
+								}
 							>
-								<Option value="dhn">Kathmandu</Option>
-								<Option value="mnr">Doti</Option>
+								<Option value="ktm">Kathmandu</Option>
+								<Option value="doti">Doti</Option>
 							</Select>
 							<label htmlFor="">
 								<h4 className="color-white">Date: </h4>
 							</label>
-							<DatePicker
-								style={{ width: '20%' }}
-								format="YYYY-MM-DD"
-								disabledDate={disabledDate}
-							/>
+							<DatePicker style={{ width: '20%' }} format="YYYY-MM-DD" disabledDate={disabledDate} />
 							<Button type="primary" icon="search" style={{ marginLeft: '1rem' }}>
 								Search
 							</Button>
