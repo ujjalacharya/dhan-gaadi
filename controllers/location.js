8756483a630670da253963432e0850fdf8ff4733
Ujjal Acharya <acharyaujjal1@gmail.com>
2019-10-29 16:48:33 +0545

    crud api of location

--- controllers/location.js
+++ controllers/location.js
@@ -1,6 +1,18 @@
 const Location = require("../models/Location");
 
-exports.addLocation = async (req, res) => {
+exports.locationByZip = async (req, res, next, zip) => {
+  const location = await Location.find({ zip });
+
+  if (!location) {
+    return res.status(400).json({
+      error: "Location not found"
+    });
+  }
+  req.location = location; // adds location object in req with location info
+  next();
+};
+
+exports.add = async (req, res) => {
   const location = new Location(req.body);
 
   await location.save();
@@ -9,7 +21,29 @@ exports.addLocation = async (req, res) => {
 };
 
 exports.getLocations = async (req, res) => {
- const location = await Location.find({}).sort({name: 1});
+  const location = await Location.find({}).sort({ name: 1 });
 
- res.json(location);
+  res.json(location);
+};
+
+exports.read = async (req, res) => {
+  res.json(req.location);
+};
+
+exports.update = async (req, res) => {
+  let location = req.location;
+
+  location = _.extend(bus, req.body);
+
+  await location.save();
+
+  res.json(location);
+};
+
+exports.remove = async (req, res) => {
+  let location = req.location;
+
+  await location.remove();
+
+  res.json({ message: "Locatioin removed successfully" });
 };
--- routes/location.js
+++ routes/location.js
@@ -1,10 +1,28 @@
-const router = require('express').Router();
-const { requireSuperadminSignin, requireOwnerSignin } = require('../controllers/auth-owner');
-const { addLocation, getLocations } = require('../controllers/location');
+const router = require("express").Router();
+const {
+  requireSuperadminSignin,
+  requireOwnerSignin
+} = require("../controllers/auth-owner");
+const {
+  add,
+  update,
+  read,
+  remove,
+  getLocations,
+  locationByZip
+} = require("../controllers/location");
 
 router
-	.route('/')
-	.get(requireOwnerSignin, getLocations)
-	.post(requireSuperadminSignin, addLocation);
+  .route("/")
+  .get(requireOwnerSignin, getLocations)
+  .post(requireSuperadminSignin, add);
+
+router
+  .route("/:zip")
+  .get(requireSuperadminSignin, read)
+  .put(requireSuperadminSignin, update)
+  .delete(requireSuperadminSignin, remove);
+
+router.param("zip", locationByZip);
 
 module.exports = router;
