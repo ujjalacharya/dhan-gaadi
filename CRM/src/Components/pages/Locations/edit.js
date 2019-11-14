166cc766a1803eb32aae74dda3168a65f2381d6c
Ujjal Acharya <acharyaujjal1@gmail.com>
2019-11-14 16:23:27 +0545

    add district dropdown on update location

--- CRM/src/Components/pages/Locations/edit.js
+++ CRM/src/Components/pages/Locations/edit.js
@@ -4,16 +4,20 @@ import Swal from "sweetalert2";
 import showError from "../../core/Error";
 import showLoading from "../../core/Loading";
 import { getALocation, updateLocation } from "../../../Utils/Requests/Location";
+import districtJson from "../../../Utils/helpers/district.json";
 
 class EditLocation extends Component {
   state = {
     error: "",
     loading: true,
-    location: {}
+    location: {},
+    districts: []
   };
 
   componentDidMount() {
     this.fetchLocation();
+    const resp = JSON.parse(JSON.stringify(districtJson));
+    this.setState({ districts: resp });
   }
 
   fetchLocation = async () => {
@@ -24,50 +28,56 @@ class EditLocation extends Component {
     });
     if (resp && resp.status === 200) {
       this.setState({
-          location: resp.data,
-          loading: false
+        location: resp.data,
+        loading: false
       });
     }
   };
 
   submit = async e => {
     e.preventDefault();
-    const { location: {_id, name, district}, error, loading } = this.state;
+    const {
+      location: { _id, name, district },
+      error,
+      loading,
+    } = this.state;
 
-      const resp = await updateLocation(
-        _id,
-        this.state.location
-      ).catch(err => {
-        this.setState({ loading: false, error: err.response.data.error });
+    const resp = await updateLocation(_id, this.state.location).catch(err => {
+      this.setState({ loading: false, error: err.response.data.error });
+    });
+    if (resp && resp.status === 200) {
+      this.setState({ loading: false });
+      Swal.fire({
+        type: "success",
+        title: "Successfully updated the location!",
+        onRender: () => {
+          this.props.history.push("/locations");
+        }
       });
-      if (resp && resp.status === 200) {
-        this.setState({ loading: false });
-        Swal.fire({
-          type: "success",
-          title: "Successfully updated the location!",
-          onRender: () => {
-            this.props.history.push("/locations");
-          }
-        });
-      }
-
+    }
   };
 
   handleChange = input => e => {
     let value = e.target.value;
 
     this.setState(prevState => ({
-        location: {
-            ...prevState.location,
-            [input]: value
-        },
-        error: ""
-    }))
+      location: {
+        ...prevState.location,
+        [input]: value
+      },
+      error: ""
+    }));
   };
 
   render() {
     const handleChange = this.handleChange;
-    const { location: {name, district}, error, loading } = this.state;
+    const {
+      location: { name, district },
+      error,
+      loading,
+      districts
+    } = this.state;
+    console.log(this.state)
     return (
       <Layout title="Update Location">
         {showError(error)}
@@ -87,14 +97,21 @@ class EditLocation extends Component {
             </div>
             <div className="form-group">
               <label>District</label>
-              <input
-                type="text"
-                className="form-control"
-                required
-                placeholder="District Name"
+              <select
+                className="custom-select custom-select-sm form-control"
                 onChange={handleChange("district")}
                 value={district}
-              />
+              >
+                <option value="Default" disabled>
+                  Select District
+                </option>
+                {districts.length > 0 &&
+                  districts.map(location => (
+                    <option value={location} key={location}>
+                      {location}
+                    </option>
+                  ))}
+              </select>
             </div>
 
             <button
