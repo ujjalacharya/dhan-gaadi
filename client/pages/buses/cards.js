ab717025c9af5ac8e10e6210383b7c07cb9e339e
Ujjal Acharya <acharyaujjal1@gmail.com>
2019-11-14 15:25:43 +0545

    show no bus found report if not found

--- client/pages/buses/cards.js
+++ client/pages/buses/cards.js
@@ -3,46 +3,52 @@ import { Row, Col } from "antd";
 import NepaliDate from "ad-bs-converter";
 
 const Cards = ({ buses }) => {
-  const nepaliDate = NepaliDate.ad2bs(
-    buses[0].journeyDate.replace("-", "/").replace("-", "/")
-  ).en;
+  const nepaliDate =
+    buses[0] &&
+    NepaliDate.ad2bs(buses[0].journeyDate.replace("-", "/").replace("-", "/"))
+      .en;
 
-  return (
-    <div className="cards">
-      <div className="card-header">
-        <h2>
-          <b>10</b> seats available in <strong>{buses.length}</strong> buses
-        </h2>
+  const markup =
+    buses.length <= 0 ? (
+      <h2>No bus found</h2>
+    ) : (
+      <div className="cards">
+        <div className="card-header">
+          <h2>
+            <b>10</b> seats available in <strong>{buses.length}</strong> buses
+          </h2>
+        </div>
+        <h4 className="card-header" style={{ color: "red" }}>
+          {`Date: ${nepaliDate.strMonth} ${nepaliDate.day}, ${nepaliDate.year}`}
+        </h4>
+        <div>
+          <hr />
+          <Row className="buses-header">
+            <Col span={4}></Col>
+            <Col span={4}>
+              <h3>Travels</h3>
+            </Col>
+            <Col span={4}>
+              <h3>Bus Type</h3>
+            </Col>
+            <Col span={4}>
+              <h3>Departure</h3>
+            </Col>
+            <Col span={4}>
+              <h3>Available</h3>
+            </Col>
+            <Col span={4}>
+              <h3>Fare</h3>
+            </Col>
+          </Row>
+          {buses.map(bus => (
+            <SingleCard key={bus._id} bus={bus} />
+          ))}
+        </div>
       </div>
-      <h4 className="card-header" style={{ color: "red" }}>
-        {`Date: ${nepaliDate.strMonth} ${nepaliDate.day}, ${nepaliDate.year}`}
-      </h4>
-      <div>
-        <hr />
-        <Row className="buses-header">
-          <Col span={4}></Col>
-          <Col span={4}>
-            <h3>Travels</h3>
-          </Col>
-          <Col span={4}>
-            <h3>Bus Type</h3>
-          </Col>
-          <Col span={4}>
-            <h3>Departure</h3>
-          </Col>
-          <Col span={4}>
-            <h3>Available</h3>
-          </Col>
-          <Col span={4}>
-            <h3>Fare</h3>
-          </Col>
-        </Row>
-        {buses.map(bus => (
-          <SingleCard key={bus._id} bus={bus} />
-        ))}
-      </div>
-    </div>
-  );
+    );
+
+  return markup;
 };
 
 export default Cards;
