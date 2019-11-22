66297d4988ff61a5fc467681c92e51585a52bf44
Ujjal Acharya <acharyaujjal1@gmail.com>
2019-11-22 17:45:31 +0545

    add customer support chat at bottom- tawkto

--- client/pages/_document.js
+++ client/pages/_document.js
@@ -1,11 +1,11 @@
 import Document, { Head, Main, NextScript } from "next/document";
 
 export default class MyDocument extends Document {
-
   render() {
     return (
       <html>
         <Head>
+          <script type="text/javascript" src="/static/script.js" />
           <link rel="stylesheet" href="/static/css/main.css" />
         </Head>
         <body>
---
+++ client/static/script.js
@@ -0,0 +1,11 @@
+var Tawk_API = Tawk_API || {},
+  Tawk_LoadStart = new Date();
+(function() {
+  var s1 = document.createElement("script"),
+    s0 = document.getElementsByTagName("script")[0];
+  s1.async = true;
+  s1.src = "https://embed.tawk.to/5dd7701743be710e1d1e9134/default";
+  s1.charset = "UTF-8";
+  s1.setAttribute("crossorigin", "*");
+  s0.parentNode.insertBefore(s1, s0);
+})();
