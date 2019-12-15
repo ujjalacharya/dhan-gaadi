77e797778c23d5788d8e2c2375670ea7e4f3212d
AcharyaUjjal1 <acharya_ujjal@yahoo.com>
2019-12-15 14:56:49 +0545

    setup redux architecture

--- app/App.js
+++ app/App.js
@@ -6,57 +6,58 @@ import { Platform, StatusBar, StyleSheet, View } from 'react-native';
 import { Ionicons } from '@expo/vector-icons';
 
 import AppNavigator from './navigation/AppNavigator';
+import { Provider } from 'react-redux';
+import store from './store';
 
 export default function App(props) {
-  const [isLoadingComplete, setLoadingComplete] = useState(false);
+	const [isLoadingComplete, setLoadingComplete] = useState(false);
 
-  if (!isLoadingComplete && !props.skipLoadingScreen) {
-    return (
-      <AppLoading
-        startAsync={loadResourcesAsync}
-        onError={handleLoadingError}
-        onFinish={() => handleFinishLoading(setLoadingComplete)}
-      />
-    );
-  } else {
-    return (
-      <View style={styles.container}>
-        {Platform.OS === 'ios' && <StatusBar barStyle="default" />}
-        <AppNavigator />
-      </View>
-    );
-  }
+	if (!isLoadingComplete && !props.skipLoadingScreen) {
+		return (
+			<AppLoading
+				startAsync={loadResourcesAsync}
+				onError={handleLoadingError}
+				onFinish={() => handleFinishLoading(setLoadingComplete)}
+			/>
+		);
+	} else {
+		return (
+			<Provider store={store}>
+				<View style={styles.container}>
+					{Platform.OS === 'ios' && <StatusBar barStyle="default" />}
+					<AppNavigator />
+				</View>
+			</Provider>
+		);
+	}
 }
 
 async function loadResourcesAsync() {
-  await Promise.all([
-    Asset.loadAsync([
-      require('./assets/images/robot-dev.png'),
-      require('./assets/images/robot-prod.png'),
-    ]),
-    Font.loadAsync({
-      // This is the font that we are using for our tab bar
-      ...Ionicons.font,
-      // We include SpaceMono because we use it in HomeScreen.js. Feel free to
-      // remove this if you are not using it in your app
-      'space-mono': require('./assets/fonts/SpaceMono-Regular.ttf'),
-    }),
-  ]);
+	await Promise.all([
+		Asset.loadAsync([require('./assets/images/robot-dev.png'), require('./assets/images/robot-prod.png')]),
+		Font.loadAsync({
+			// This is the font that we are using for our tab bar
+			...Ionicons.font,
+			// We include SpaceMono because we use it in HomeScreen.js. Feel free to
+			// remove this if you are not using it in your app
+			'space-mono': require('./assets/fonts/SpaceMono-Regular.ttf'),
+		}),
+	]);
 }
 
 function handleLoadingError(error) {
-  // In this case, you might want to report the error to your error reporting
-  // service, for example Sentry
-  console.warn(error);
+	// In this case, you might want to report the error to your error reporting
+	// service, for example Sentry
+	console.warn(error);
 }
 
 function handleFinishLoading(setLoadingComplete) {
-  setLoadingComplete(true);
+	setLoadingComplete(true);
 }
 
 const styles = StyleSheet.create({
-  container: {
-    flex: 1,
-    backgroundColor: '#fff',
-  },
+	container: {
+		flex: 1,
+		backgroundColor: '#fff',
+	},
 });
---
+++ app/store.js
@@ -0,0 +1,15 @@
+import { createStore, applyMiddleware } from 'redux';
+import thunk from 'redux-thunk';
+import rootReducer from './reducers';
+
+const initialState = {};
+
+const middleware = [thunk];
+
+const store = createStore(
+  rootReducer,
+  initialState,
+  applyMiddleware(...middleware)
+);
+
+export default store;
