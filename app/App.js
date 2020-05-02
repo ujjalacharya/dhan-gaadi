256530ce841624c7992732cc756d13495acf7f94
Ujjal Acharya <ujjal.acharya@amniltech.com>
2020-05-03 02:22:46 +0545

    make new app

--- app/App.js
+++ app/App.js
@@ -1,63 +1,28 @@
-import { AppLoading } from 'expo';
-import { Asset } from 'expo-asset';
-import * as Font from 'expo-font';
-import React, { useState } from 'react';
-import { Platform, StatusBar, StyleSheet, View } from 'react-native';
-import { Ionicons } from '@expo/vector-icons';
+import React from "react";
+import { Provider } from "react-redux";
+import { createStore, applyMiddleware, compose } from "redux";
 
-import AppNavigator from './navigation/AppNavigator';
-import { Provider } from 'react-redux';
-import store from './store';
+import promiseMiddleware from "redux-promise";
 
-export default function App(props) {
-	const [isLoadingComplete, setLoadingComplete] = useState(false);
+import Main from "./src";
 
-	if (!isLoadingComplete && !props.skipLoadingScreen) {
-		return (
-			<AppLoading
-				startAsync={loadResourcesAsync}
-				onError={handleLoadingError}
-				onFinish={() => handleFinishLoading(setLoadingComplete)}
-			/>
-		);
-	} else {
-		return (
-			<Provider store={store}>
-				<View style={styles.container}>
-					{Platform.OS === 'ios' && <StatusBar barStyle="default" />}
-					<AppNavigator />
-				</View>
-			</Provider>
-		);
-	}
+import reducers from "./src/store/reducers";
+
+const composeEnhancers = window.__REDUX_DEVTOOLS_EXTENSION_COMPOSE__ || compose;
+
+const createStoreWithMiddleware = createStore(
+  reducers,
+  composeEnhancers(applyMiddleware(promiseMiddleware))
+);
+
+class App extends React.Component {
+  render() {
+    return (
+      <Provider store={createStoreWithMiddleware}>
+        <Main />
+      </Provider>
+    );
+  }
 }
 
-async function loadResourcesAsync() {
-	await Promise.all([
-		Asset.loadAsync([require('./assets/images/robot-dev.png'), require('./assets/images/robot-prod.png')]),
-		Font.loadAsync({
-			// This is the font that we are using for our tab bar
-			...Ionicons.font,
-			// We include SpaceMono because we use it in HomeScreen.js. Feel free to
-			// remove this if you are not using it in your app
-			'space-mono': require('./assets/fonts/SpaceMono-Regular.ttf'),
-		}),
-	]);
-}
-
-function handleLoadingError(error) {
-	// In this case, you might want to report the error to your error reporting
-	// service, for example Sentry
-	console.warn(error);
-}
-
-function handleFinishLoading(setLoadingComplete) {
-	setLoadingComplete(true);
-}
-
-const styles = StyleSheet.create({
-	container: {
-		flex: 1,
-		backgroundColor: '#fff',
-	},
-});
+export default App;
