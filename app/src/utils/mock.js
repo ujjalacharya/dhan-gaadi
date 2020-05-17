bd2aa291d4b41cc71bf60ee1f5deed71473d6722
Ujjal Acharya <ujjal.acharya@amniltech.com>
2020-05-17 15:09:48 +0545

    add data on mockdata

--- app/src/utils/mock.js
+++ app/src/utils/mock.js
@@ -57,25 +57,33 @@ export const busData = [
     {
         id: 1,
         title: "Khaptad Delux",
-        image: "https://lh3.googleusercontent.com/proxy/QGBUViDMviDS6LmtW8DsXzSxGn1UsDe6YCaNHBF7u39i7ngk3gbJuVWbCtG-9vb-Qa_IQV-V60jUp7QwwHbFs5oS4g7UjaSYAcaCV7UsKKHBnPhgHrmJLDaY-2iKdYC2ww",
-        price: "Rs 1600"
+        image: "https://www.yatranepal.com/images/dhangadi-bus/dhangadi-bus-02.jpg",
+        seats: 14,
+        price: "Rs 1600",
+        time: "1PM"
     },
     {
         id: 1,
         title: "Khaptad Air Condition",
         image: "https://worldhimalayandestination.com/wp-content/uploads/2019/06/tourist-bus-825x450.jpg",
-        price: "Rs 2000"
+        seats: 11,
+        price: "Rs 2000",
+        time: "3PM"
     },
     {
         id: 1,
         title: "Khaptad Suspension",
         image: "https://encrypted-tbn0.gstatic.com/images?q=tbn%3AANd9GcSObRM0s5pOI2ODbdshb0hchMlhRO4SXxp32MrxVUA6jMzUelUU&usqp=CAU",
-        price: "Rs 1400"
+        seats: 10,
+        price: "Rs 1400",
+        time: "5PM"
     },
     {
         id: 1,
         title: "Khaptad Normal",
         image: "https://encrypted-tbn0.gstatic.com/images?q=tbn%3AANd9GcQvYXA4bxfAD5ByqAbc7je7DIOm1I9AyTZcoh3t8Ya2KT0WhOvT&usqp=CAU",
-        price: "Rs 1000"
+        seats: 12,
+        price: "Rs 1000",
+        time: "2PM"
     },
 ]
