b5c16e73bc38193ef9c6441e8779546caee92c8f
Ujjal Acharya <acharyaujjal1@gmail.com>
2022-09-14 11:19:38 +0545

    CRM:Add new Date on footer

--- CRM/src/Components/core/Footer.js
+++ CRM/src/Components/core/Footer.js
@@ -1,17 +1,19 @@
-import React from 'react';
+import React from "react";
 
 const Footer = () => {
-	return (
-		<div className="box-footer">
-			<div className="row">
-				<div>
-					<div className="description-block border-right">
-						<h5 className="description-header">&copy; 2020 Dhan-gaadi</h5>
-					</div>
-				</div>
-			</div>
-		</div>
-	);
+  return (
+    <div className="box-footer">
+      <div className="row">
+        <div>
+          <div className="description-block border-right">
+            <h5 className="description-header">
+              &copy; {new Date().getFullYear()} Dhan-gaadi
+            </h5>
+          </div>
+        </div>
+      </div>
+    </div>
+  );
 };
 
 export default Footer;
